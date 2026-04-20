4a423dc303d152fbcc31c7cf85f46047319e7a83
Alexander James Phillips <ajp.global@gmail.com>
2026-04-20 23:07:59 +0100

    Reverse attempt at spliting vendor and application code

--- vite.config.ts
+++ vite.config.ts
@@ -19,15 +19,26 @@ export default defineConfig({
         outDir: "../dist", // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true, // Add sourcemap
-        rollupOptions:
-        {
-            output:
-            {
-                manualChunks(id) {
-                    if (id.includes("node_modules")) return "vendor"
-                },
-            },
-        },
+        // This does splitting into application and vendor code does not work yet.
+        // I think what is happening is that the index-*.js file is being served
+        // correctly by the deno server from:
+        //     https://wikisim-server.wikisim.deno.net/1272v4/assets/index-DfbjeLe0.js
+        // which returns with a redirect to a supabase URL like:
+        //     https://sfkgqscbwofiphfxhnxg.supabase.co/storage/v1/object/public/interactables_files/3c378f9f294eb84b22d64bd17e09ce0dee56d37d3fdab6e7b7d64210d9c13a6c
+        // but then because it references ./vendor-*.js this file is requested
+        // from the new index-*.js file path, i.e.
+        //     https://sfkgqscbwofiphfxhnxg.supabase.co/vendor-Cm_jY_mP.js
+        // and that is not being found, instead of:
+        //     https://wikisim-server.wikisim.deno.net/1272v4/assets/vendor-Cm_jY_mP.js
+        // rollupOptions:
+        // {
+        //     output:
+        //     {
+        //         manualChunks(id) {
+        //             if (id.includes("node_modules")) return "vendor"
+        //         },
+        //     },
+        // },
     },
     plugins:
     [
