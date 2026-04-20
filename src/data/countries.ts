71639d8d5f17a32f213e1ccaa00cfff1b9bb9996
Alexander James Phillips <ajp.global@gmail.com>
2026-04-20 13:00:23 +0100

    Add some manual votes and order countries by name

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -14,12 +14,18 @@ export interface CountryData
 const implemented_countries: Set<CountryISO2Code> = new Set([
     "GB",
 ])
+const existing_manual_votes: Partial<Record<CountryISO2Code, number>> = {
+    "GB": 7,
+    "IN": 1,
+    "FR": 1,
+}
 
 export const extended_countries_data: CountryData[] = countries_data.map(c => ({
     ...c,
     implemented: implemented_countries.has(c.code2),
-    votes: 0,
+    votes: existing_manual_votes[c.code2] ?? 0,
 }))
+.sort((a, b) => a.name.localeCompare(b.name)) // sort alphabetically by name
 
 export function get_country_by_code(code2: CountryISO2Code): CountryData | undefined
 {
