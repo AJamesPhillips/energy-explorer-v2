4586a450880be6766c6c1be939cdc7df181c128b
Alexander James Phillips <ajp.global@gmail.com>
2026-04-21 22:32:37 +0100

    Simplify on boarding message

--- src/sim_3d/simple_sim/WelcomeMessage.tsx
+++ src/sim_3d/simple_sim/WelcomeMessage.tsx
@@ -7,21 +7,21 @@ export function WelcomeMessage()
         id="simple_sim_welcome_message"
         message={
             <>
-                <h1>⚡️Simple Sim⚡️</h1>
+                <h1>⚡️Power the UK⚡️<br/>On Renewables Alone</h1>
 
-                <p>
+                {/* <p>
                     This is a simplification of the UK's national energy system.
-                </p>
+                </p> */}
                 <p style={{ backgroundColor: "#fffae6", padding: 8, borderRadius: 4, border: "1px solid #ffe58f" }}>
                     Click on the grid
                     to add different energy sources.
                 {/* And click on ℹ️ symbols to get more information */}
                 </p>
 
-                <p>
+                {/* <p>
                     Why?  To get a better intuitive understanding of the scale of
                     energy demand and of the area, and resources required for different sources of energy.
-                </p>
+                </p> */}
 
 
                 {/* <p>
@@ -29,14 +29,6 @@ export function WelcomeMessage()
                     sea in the UK's exclusive economic zone is shown, the real area of the UK
                     land and sea is twice as large!
                 </p> */}
-
-                <p>
-                    If you enjoy this please share it.<br />
-                    If you want to get involved you can <a href="mailto:hello@wikisim.org">email us, </a>
-                    <a href="https://wikisim.org/wiki/1272">check the data, </a>
-                    <a href="https://github.com/AJamesPhillips/energy-explorer-v2/issues">add code, </a>
-                    or <a href="https://www.patreon.com/WikiSim">donate ❤️</a>
-                </p>
             </>
         }
     />
