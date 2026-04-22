68a320e8b36e6d164fafe50b4a3e7634f206a8d3
Alexander James Phillips <ajp.global@gmail.com>
2026-04-22 23:48:30 +0100

    Add icons to footer text

--- src/components/svgs.tsx
+++ src/components/svgs.tsx
@@ -1,20 +1,40 @@
+import { CSSProperties } from "react"
+
 import { asset_url } from "../utils/asset_url"
 const bluesky_logo_url = asset_url("svgs/bluesky.svg")
 const github_logo_url = asset_url("svgs/github.svg")
 const mail_logo_url = asset_url("svgs/mail.svg")
 
 
+const default_style: CSSProperties = {
+    display: "inline-block",
+    verticalAlign: "center",
+}
+
+
 export function BlueSkyLogo(props: { height?: number })
 {
-    return <img src={bluesky_logo_url} style={{ height: props.height }} />
+    return <img src={bluesky_logo_url} style={{ ...default_style, height: props.height }} />
 }
 
 export function GitHubLogo(props: { height?: number })
 {
-    return <img src={github_logo_url} style={{ height: props.height }} />
+    return <img src={github_logo_url} style={{ ...default_style, height: props.height }} />
+}
+
+export function MagnifyingGlassLogo(props: { height?: number })
+{
+    return <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        style={{ ...default_style, height: props.height }}
+    >
+        <path d="M10 2a8 8 0 015.292 13.708l4.147 4.146a1 1 0 01-1.414 1.414l-4.146-4.147A8 8 0 1110 2zm0 2a6 6 0 100 12A6 6 0 0010 4z" />
+    </svg>
 }
 
 export function MailLogo(props: { height?: number })
 {
-    return <img src={mail_logo_url} style={{ height: props.height }} />
+    return <img src={mail_logo_url} style={{ ...default_style, height: props.height }} />
 }
--- src/sim_3d/simple_sim/FooterLinks.tsx
+++ src/sim_3d/simple_sim/FooterLinks.tsx
@@ -1,3 +1,4 @@
+import { GitHubLogo, MagnifyingGlassLogo, MailLogo } from "../../components/svgs"
 import "./FooterLinks.css"
 
 
@@ -5,9 +6,9 @@ export function FooterLinks()
 {
     return <div id="footer_links">
         If you enjoy this please share it.
-        If you want to get involved you can <a href="mailto:hello@wikisim.org">email us</a>,{" "}
-        <a href="https://wikisim.org/wiki/1272">check the data</a>,{" "}
-        <a href="https://github.com/AJamesPhillips/energy-explorer-v2/issues">add code</a>,{" "}
+        If you want to get involved you can <a href="mailto:hello@wikisim.org">email us <MailLogo height={14} /></a>,{" "}
+        <a href="https://wikisim.org/wiki/1272">check the data <MagnifyingGlassLogo height={14} /></a>,{" "}
+        <a href="https://github.com/AJamesPhillips/energy-explorer-v2/issues">add code <GitHubLogo height={14} /></a>,{" "}
         or <a href="https://www.patreon.com/WikiSim">donate to support us to do more ❤️</a>
     </div>
 }
