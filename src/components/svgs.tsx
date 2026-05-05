3f7b4d54f1b7810f9a844d93cc484c88576b34ab
Alexander James Phillips <ajp.global@gmail.com>
2026-05-05 11:19:58 +0100

    Refactor handling SVGs

--- src/components/svgs.tsx
+++ src/components/svgs.tsx
@@ -2,18 +2,18 @@ import { CSSProperties } from "react"
 
 import { asset_url } from "../utils/asset_url"
 const bluesky_logo_url = asset_url("svgs/bluesky.svg")
+const close_icon_url = asset_url("svgs/close.svg")
 const github_logo_url = asset_url("svgs/github.svg")
-const graph_logo_url = asset_url("svgs/graph.svg")
-const info_logo_url = asset_url("svgs/info.svg")
-const magnifying_logo_url = asset_url("svgs/magnifying_glass.svg")
-const mail_logo_url = asset_url("svgs/mail.svg")
+const graph_icon_url = asset_url("svgs/graph.svg")
+const info_icon_url = asset_url("svgs/info.svg")
+const magnifying_icon_url = asset_url("svgs/magnifying_glass.svg")
+const mail_icon_url = asset_url("svgs/mail.svg")
 
 
 const default_style: CSSProperties = {
     display: "inline-block",
     verticalAlign: "center",
 }
-
 
 export function BlueSkyLogo(props: { height?: number })
 {
@@ -25,22 +25,32 @@ export function GitHubLogo(props: { height?: number })
     return <img src={github_logo_url} style={{ ...default_style, height: props.height }} />
 }
 
-export function GraphLogo(props: { style?: CSSProperties })
-{
-    return <img src={graph_logo_url} style={{ ...default_style, ...props.style }} />
+const default_icon_style: CSSProperties = {
+    ...default_style,
+    cursor: "pointer",
 }
 
-export function InfoLogo(props: { style?: CSSProperties })
+export function CloseIcon(props: { style?: CSSProperties })
 {
-    return <img src={info_logo_url} style={{ ...default_style, ...props.style }} />
+    return <img src={close_icon_url} style={{ ...default_icon_style, ...props.style }} />
 }
 
-export function MagnifyingGlassLogo(props: { height?: number })
+export function GraphIcon(props: { style?: CSSProperties })
 {
-    return <img src={magnifying_logo_url} style={{ ...default_style, height: props.height }} />
+    return <img src={graph_icon_url} style={{ ...default_icon_style, ...props.style }} />
 }
 
-export function MailLogo(props: { style?: CSSProperties })
+export function InfoIcon(props: { style?: CSSProperties })
 {
-    return <img src={mail_logo_url} style={{ ...default_style, ...props.style }} />
+    return <img src={info_icon_url} style={{ ...default_icon_style, ...props.style }} />
+}
+
+export function MagnifyingGlassIcon(props: { height?: number })
+{
+    return <img src={magnifying_icon_url} style={{ ...default_icon_style, height: props.height }} />
+}
+
+export function MailIcon(props: { style?: CSSProperties })
+{
+    return <img src={mail_icon_url} style={{ ...default_icon_style, ...props.style }} />
 }
